3da4a89f842eaf25afbf772c4ec86cb39500adb8
AtharvaCM <necrons999@gmail.com>
2022-11-10 15:24:04 +0530

    staticProps dealerListings added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
+
+import { GET_DEALER_LISTINGS } from "@/queries/get-dealer-listings";
 
 // custom components
 import Layout from "@/components/layout";
+import client from "apollo-client";
 
-const Home: NextPage = () => {
+type DealerListingsType = {
+  colour: string;
+  engineSize: number;
+  fuelType: string;
+  id: number;
+  image: string;
+  kms: number;
+  listType: string;
+  makeName: string;
+  modelName: string;
+  price: number;
+  region: { regionName: string; __typename: string };
+  seat_capacity: number;
+  state: string;
+  transmission: string;
+  variant: string;
+  vehicleKey: string;
+  year: number;
+  __typename: "DealerListing";
+};
+
+type HomePagePropsType = {
+  data: DealerListingsType[];
+};
+
+const Home: NextPage<HomePagePropsType> = ({ data }) => {
+  console.log("data: ", data);
   return (
     <Layout>
       <section id="cars-for-sale" className="mx-[10%]">
@@ -22,3 +51,13 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client.query(GET_DEALER_LISTINGS);
+
+  return {
+    props: {
+      data: data.DealerListings.results,
+    },
+  };
+};
