3e5188127213357737fff9353dd5c4355910ef6a
AtharvaCM <necrons999@gmail.com>
2022-11-15 13:39:43 +0530

    made the card dynamic

--- src/components/UI/card/index.tsx
+++ src/components/UI/card/index.tsx
@@ -1,28 +1,37 @@
 import React from "react";
+import Button from "../button";
+import ButtonOutline from "../button-outline";
 
-const Card: React.FC = () => {
+type CardPropsType = {
+  src: string;
+  alt: string;
+  cardTitle: string;
+  content: string;
+  btnTitle: string;
+  btnOutlineTitle: string;
+};
+
+const Card: React.FC<CardPropsType> = ({
+  src,
+  alt,
+  cardTitle,
+  content,
+  btnTitle,
+  btnOutlineTitle,
+}) => {
   return (
     <div className="flex justify-center">
       <div className="rounded-lg shadow-lg bg-white max-w-sm">
         <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-          <img
-            className="rounded-t-lg"
-            src="https://mdbootstrap.com/img/new/standard/nature/182.jpg"
-            alt=""
-          />
+          <img className="rounded-t-lg" src={src} alt={alt} />
         </a>
         <div className="p-6">
-          <h5 className="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-          <p className="text-gray-700 text-base mb-4">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <button
-            type="button"
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-          >
-            Button
-          </button>
+          <h5 className="text-gray-900 text-xl font-medium mb-2">
+            {cardTitle}
+          </h5>
+          <p className="text-gray-700 text-base mb-4">{content}</p>
+          <Button title={btnTitle} />
+          <ButtonOutline title={btnOutlineTitle} />
         </div>
       </div>
     </div>
