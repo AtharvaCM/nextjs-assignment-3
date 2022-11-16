cc89813cf955febb6b2daf9ea58e95fa990a5872
AtharvaCM <necrons999@gmail.com>
2022-11-16 16:01:09 +0530

    dynamically import tw wlements on load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import "@/styles/globals.css";
 import "@/styles/header.css";
+
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+
 import { ApolloProvider } from "@apollo/client";
 import client from "apollo-client";
-import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    async function loadData() {
+      await import("tw-elements");
+    }
+    loadData();
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <Component {...pageProps} />
