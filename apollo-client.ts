cdd5355b5bfa27be9c73e201b8453dff78d39ca8
AtharvaCM <necrons999@gmail.com>
2022-11-16 13:42:59 +0530

    env var changed

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.DRIVE_GRAPHQL_URI,
+  uri: process.env.NEXT_PUBLIC_DRIVE_GRAPHQL_URI,
   cache: new InMemoryCache(),
 });
 
