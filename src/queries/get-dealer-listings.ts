3756d90aa728f60cb7a0d109a0c8a0df3c3e3226
AtharvaCM <necrons999@gmail.com>
2022-11-16 10:04:17 +0530

    loadmore query added

--- src/queries/get-dealer-listings.ts
+++ src/queries/get-dealer-listings.ts
@@ -3,7 +3,7 @@ import { gql, OperationVariables, QueryOptions } from "@apollo/client";
 export const GET_DEALER_LISTINGS: QueryOptions<OperationVariables, any> = {
   query: gql`
     query GetDealerListings {
-      DealerListings {
+      DealerListings(paginate: { page: 1, pageSize: 9 }) {
         results {
           id
           colour
@@ -25,7 +25,50 @@ export const GET_DEALER_LISTINGS: QueryOptions<OperationVariables, any> = {
             regionName: region_name
           }
         }
+        pageInfo {
+          hasNextPage
+          currentPage
+          pageCount
+          itemCount
+        }
       }
     }
   `,
 };
+
+export const LOAD_MORE_DEALER_LISTINGS: QueryOptions<OperationVariables, any> =
+  {
+    query: gql`
+      query GetMoreDealerListings($page: Int) {
+        DealerListings(paginate: { page: $page, pageSize: 9 }) {
+          results {
+            id
+            colour
+            vehicleKey: vehicle_key
+            listType: list_type
+            year
+            price: price_excluding_government_charges
+            kms: odometer
+            variant
+            fuelType: fuel_type
+            transmission
+            engineSize: engine_size
+            seat_capacity
+            image: main_image
+            state: location_state
+            makeName: make
+            modelName: model
+            region: Region {
+              regionName: region_name
+            }
+          }
+          pageInfo {
+            hasNextPage
+            currentPage
+            pageCount
+            itemCount
+          }
+        }
+      }
+    `,
+  };
