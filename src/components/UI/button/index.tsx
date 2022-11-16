a019f89e96bab589f7e44088ec18daac4bd1a9b3
AtharvaCM <necrons999@gmail.com>
2022-11-16 09:34:57 +0530

    click handler added

--- src/components/UI/button/index.tsx
+++ src/components/UI/button/index.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 type ButtonPropsType = {
   title: string;
+  onClick: () => void;
 };
 
-const Button: React.FC<ButtonPropsType> = ({ title }) => {
+const Button: React.FC<ButtonPropsType> = ({ title, onClick }) => {
   return (
     <button
       type="button"
-      className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+      onClick={onClick}
+      className=" inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
     >
       {title}
     </button>
