f512ed02181e5214e4e462139df19256f48f59a5
AtharvaCM <necrons999@gmail.com>
2022-11-21 15:47:17 +0530

    added onclick callback

--- src/components/UI/button-outline/index.tsx
+++ src/components/UI/button-outline/index.tsx
@@ -2,13 +2,18 @@ import React from "react";
 
 type ButtonOutlinePropsType = {
   title: string;
+  onClick?: () => void;
 };
 
-const ButtonOutline: React.FC<ButtonOutlinePropsType> = ({ title }) => {
+const ButtonOutline: React.FC<ButtonOutlinePropsType> = ({
+  title,
+  onClick,
+}) => {
   return (
     <button
       type="button"
-      className="inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+      onClick={onClick}
+      className="inline-block rounded border-2 border-blue-600 px-6 py-2 text-xs font-medium uppercase leading-tight text-blue-600 transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
     >
       {title}
     </button>
